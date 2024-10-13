11905eaac560b785bb1bfb177b300832c6fe08a3
Sungho Baek <ashsh0412@gmail.com>
2024-10-12 22:09:59 -0400

    user application created

---
+++ users/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "username",
+                    "password",
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "is_rider",
+                ),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login", "date_joined"),
+            },
+        ),
+    )
+    list_display = ("username","first_name", "last_name", "is_rider")

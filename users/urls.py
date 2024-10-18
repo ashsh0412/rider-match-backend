d67a302135838a98872ea95c93b86da9797f9f02
Sungho Baek <ashsh0412@gmail.com>
2024-10-18 19:50:31 -0400

    urls

--- users/urls.py
+++ users/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("", Users.as_view()),
     path("me", Me.as_view()),
     path("change-password", ChangePassword.as_view()),
-    path("log-in", LogIn.as_view()),
+    path("log-in", LogIn.as_view(), name="login"),
     path("log-out", LogOut.as_view()),
     path("token_login", obtain_auth_token),
     path("jwt_login", JWTLogIn.as_view()),

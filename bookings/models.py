161b00d74608c2567408fe89dc5b9a2c4ee75858
Sungho Baek <ashsh0412@gmail.com>
2024-12-27 21:13:39 -0600

    character update

--- bookings/models.py
+++ bookings/models.py
@@ -10,14 +10,14 @@ class Booking(models.Model):
         null=True,
         blank=True,
     )
-    driver_name = models.CharField(max_length=200, null=True, blank=True)
+    driver_name = models.CharField(max_length=1000, null=True, blank=True)
     passengers = models.JSONField(null=True, blank=True)  # [{"id": 1, "name": "John"}]
     pickup_times = models.JSONField(null=True, blank=True)  # ["2024-03-15T14:30"]
     locations = models.JSONField(null=True, blank=True)
     guests = models.PositiveSmallIntegerField(default=1)
     created_at = models.DateTimeField(default=timezone.now)
     arrival_time = models.DateTimeField(default=timezone.now)
-    starting_point = models.CharField(max_length=200, null=True, blank=True)
+    starting_point = models.CharField(max_length=1000, null=True, blank=True)
     map_url = models.URLField(null=True, blank=True)
 
     def __str__(self):
--- locations/models.py
+++ locations/models.py
@@ -14,12 +14,12 @@ class Location(models.Model):
     end_latitude = models.FloatField()
     end_longitude = models.FloatField()
     pickup_location = models.CharField(
-        max_length=300,
+        max_length=1000,
         null=True,
         blank=True,
     )
     dropoff_location = models.CharField(
-        max_length=300,
+        max_length=1000,
         null=True,
         blank=True,
     )
